f5a937dc5fd18395acaaa9ff5acf9b1436e84c23
Gerard Braad <me@gbraad.nl>
2021-08-02 14:16:35 +0800

    Basic functionality

--- commander.js
+++ commander.js
@@ -3,12 +3,13 @@ const got = require('got');
 module.exports = class DaemonCommander {
 
     constructor() {
+      this.apiPath = 'http://unix://?/pipe/crc-http:/api';
 
     }
 
     async status() {
        const options = {
-          url: `http://unix://?/pipe/crc-http:/api/status`,
+          url: this.apiPath + `/status`,
           method: 'GET'
         };
 
@@ -18,15 +19,40 @@ module.exports = class DaemonCommander {
         return status.CrcStatus;
     }
 
-    start() {
+    async start() {
+       const options = {
+          url: this.apiPath + `/start`,
+          method: 'GET'
+        };
+
+        const {body} = await got(options);
+        var status = JSON.parse(body);
+        
         return "Starting";
     }
 
-    stop() {
+    async stop() {
+       const options = {
+          url: this.apiPath + `/stop`,
+          method: 'GET'
+        };
+
+        const {body} = await got(options);
+        var status = JSON.parse(body);
+    
+
         return "Stopping";
     }
 
-    delete() {
+    async delete() {
+       const options = {
+          url: this.apiPath + `/delete`,
+          method: 'GET'
+        };
+
+        const {body} = await got(options);
+        var status = JSON.parse(body);
+
         return "Deleting";
     }
 }
--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const commander = new DaemonCommander()
 
 const start = async function() {
 
-  document.querySelector('#stop').onclick= function(e) {
-    statusDiv.innerHTML = commander.stop();
+  document.querySelector('#stop').onclick = async() => {
+    statusDiv.innerHTML = await commander.stop();
   }
 
-  document.querySelector('#start').onclick= function(e) {
-    statusDiv.innerHTML = commander.start();
+  document.querySelector('#start').onclick = async() => {
+    statusDiv.innerHTML = await commander.start();
   }
 
-  document.querySelector('#delete').onclick= function(e) {
-    statusDiv.innerHTML = commander.delete();
+  document.querySelector('#delete').onclick = async() => {
+    statusDiv.innerHTML = await commander.delete();
   }
 
   while(true) {
     statusDiv.innerHTML = await commander.status();
     await delay(1000);
   }
-  
+
 }
 
 start();
