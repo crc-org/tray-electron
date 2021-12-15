31f07d178b9643f93202d4b742adaa0e7f40b326
Gerard Braad <me@gbraad.nl>
2021-12-15 17:55:30 +0800

    Import shared styling for components

--- frontend/src/components/MiniStatusWindow.jsx
+++ frontend/src/components/MiniStatusWindow.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {
-  Bullseye
-} from '@patternfly/react-core';
+
 import {
   ControlCard
 } from '@gbraad/crc-react-components';
+import '@gbraad/crc-react-components/dist/index.css';
 
 export default class MiniStatusWindow extends React.Component {
   constructor(props) {
