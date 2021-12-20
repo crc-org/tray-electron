fb5a263c1b9cb69fdded3206566497f1d367456c
Gerard Braad <me@gbraad.nl>
2021-12-20 14:57:46 +0800

    Add basic log

--- frontend/src/components/StatusWindow.jsx
+++ frontend/src/components/StatusWindow.jsx
@@ -3,7 +3,8 @@ import {
   Bullseye
 } from '@patternfly/react-core';
 import {
-  ControlCard
+  ControlCard,
+  LogWindow
 } from '@code-ready/crc-react-components';
 import '@code-ready/crc-react-components/dist/index.css';
 
@@ -11,7 +12,12 @@ export default class StatusWindow extends React.Component {
   constructor(props) {
     super(props);
 
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+
     this.control = React.createRef();
+    this.logWindow = React.createRef();
   }
 
   componentDidMount() {
@@ -20,26 +26,41 @@ export default class StatusWindow extends React.Component {
     })
   }
 
+  log(message) {
+    this.logWindow.current.log(message);
+  }
+
   onStart() {
+    this.log("→ Start clicked");
+
     window.api.startInstance({})
   }
 
   onStop() {
+    this.log("→ Stop clicked");
+
     window.api.stopInstance({})
   }
 
   onDelete() {
+    this.log("→ Delete clicked");
+
     window.api.deleteInstance({})
   }
 
   render() {
     return (
-    <Bullseye>
-        <ControlCard ref={this.control}
-          onStartClicked={this.onStart}
-          onStopClicked={this.onStop}
-          onDeleteClicked={this.onDelete} />
-    </Bullseye>
+      <>
+        <Bullseye>
+          <ControlCard ref={this.control}
+            onStartClicked={this.onStart}
+            onStopClicked={this.onStop}
+            onDeleteClicked={this.onDelete} />
+        </Bullseye>
+        <Bullseye>
+          <LogWindow cols={94} rows={12} ref={this.logWindow} />
+        </Bullseye>
+      </>
     );
   }
 }
