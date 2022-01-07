crc-org/tray-electron

commit 10c5afac6c8d2cdea71a3abcbbe0bb8636386e8b

use correct segment write key for the packaged version

telemetry.js
@@ -10,6 +10,10 @@ let writeKey = 'R7jGNYYO5gH0Nl5gDlMEuZ3gPlDJKQak'; // test
 let userIdHashPath = path.join(__dirname, "segmentIdentityHash");
 let userIdPath = path.join(os.homedir(), '.redhat', 'anonymousId');
 
+if (app.isPackaged) {
+    writeKey = 'cvpHsNcmGCJqVzf6YxrSnVlwFSAZaYtp';
+}
+
 module.exports = class Telemetry {
     #traits = {
         tray_os_version: os.version(), // Windows 10 Pro

commit 4ab1ab76d8eb52b71a21a792a27219d1573119e7

use electron ipc to send telemetry from the renderer processes
this fixes the bug that was introduced in 10c5afac6c8d2cdea71a3abcbbe0bb8636386e8b

main.js
@@ -19,7 +19,7 @@ const showNotification = require('./notification');
 
 const config = new Config()
 // create the telemetry object
-const telemetry = new Telemetry(config.get('enableTelemetry'))
+const telemetry = new Telemetry(config.get('enableTelemetry'), getSegmentWriteKey())
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -51,6 +51,13 @@ function crcBinary() {
 
   // No path provided
   return "crc"
+}
+
+function getSegmentWriteKey() {
+  if (app.isPackaged) {
+    return 'cvpHsNcmGCJqVzf6YxrSnVlwFSAZaYtp'
+  }
+  return 'R7jGNYYO5gH0Nl5gDlMEuZ3gPlDJKQak'
 }
 
 /* ----------------------------------------------------------------------------
@@ -628,3 +635,15 @@ ipcMain.on('logs-retrieve', async (event, args) => {
     await delay(3000);
   }
 });
+
+/* ----------------------------------------------------------------------------
+// Telemetry
+// ------------------------------------------------------------------------- */
+
+ipcMain.on('track-error', async (event, arg) => {
+  telemetry.trackError(arg)
+})
+
+ipcMain.on('track-success', async (event, arg) => {
+  telemetry.trackSuccess(arg)
+})

preload-main.js
@@ -1,10 +1,4 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const Config = require('./config');
-const Telemetry = require('./telemetry');
-
-
-const config = new Config()
-const telemetry = new Telemetry(config.get('enableTelemetry'))
 
 contextBridge.exposeInMainWorld('api', {
   closeActiveWindow: () => { 
@@ -61,11 +55,11 @@ contextBridge.exposeInMainWorld('api', {
 
   telemetry: {
     trackError: (errorMsg) => {
-      telemetry.trackError(errorMsg);
+      ipcRenderer.send('track-error', errorMsg);
     },
 
     trackSuccess: (successMsg) => {
-      telemetry.trackSuccess(successMsg);
+     ipcRenderer.send('track-success', successMsg);
     }
   }
 });

telemetry.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const {v4: uuidv4 } = require('uuid');
 const hash = require('object-hash');
 
-let writeKey = 'R7jGNYYO5gH0Nl5gDlMEuZ3gPlDJKQak'; // test
 let userIdHashPath = path.join(os.homedir(), '.crc', "segmentIdentityHash");
 let userIdPath = path.join(os.homedir(), '.redhat', 'anonymousId');
 
@@ -25,7 +24,7 @@ module.exports = class Telemetry {
         ip: "0.0.0.0"
     }
 
-    constructor(telemetryEnabled) {
+    constructor(telemetryEnabled, writeKey) {
         this.telemetryEnabled = telemetryEnabled
         
         if (!telemetryEnabled) {