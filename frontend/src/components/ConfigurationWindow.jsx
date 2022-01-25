2d46d77b179add67dcc2257cd88ed5479d513847
Gerard Braad <me@gbraad.nl>
2022-01-25 15:51:05 +0800

    Default props are ignored

--- frontend/src/components/ConfigurationWindow.jsx
+++ frontend/src/components/ConfigurationWindow.jsx
@@ -37,7 +37,7 @@ export default class ConfigurationWindow extends React.Component {
 
     configurationSave(data) {
         window.api.configurationSave(data)
-        
+
         window.close();
     }
 
