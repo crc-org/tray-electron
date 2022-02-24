84dc9700d57b08d2a026dfb8d83fe8b1dae21098
Gerard Braad <me@gbraad.nl>
2022-02-24 19:16:44 +0800

    Change fraction precision

--- frontend/src/components/OpenShiftStatus.tsx
+++ frontend/src/components/OpenShiftStatus.tsx
@@ -31,8 +31,8 @@ export class OpenShiftStatus extends React.Component {
 
     formatSize(bytes: number): string {
         const i = Math.floor(Math.log(bytes) / Math.log(1024));
-        return (!bytes && '0 Bytes') ||
-            (bytes / Math.pow(1024, i)).toFixed(2) + " " + ['Bytes', 'KB', 'MB', 'GB', 'TB'][i];
+        return (!bytes && '0 B') ||
+            (bytes / Math.pow(1024, i)).toFixed(1) + " " + ['B', 'KB', 'MB', 'GB', 'TB'][i];
     }
 
     updateState(key: string, value: unknown): void {
