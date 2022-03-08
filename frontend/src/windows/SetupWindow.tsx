a18ef19eec9af2dfc98b757b474ac6140e8d7e3d
Gerard Braad <me@gbraad.nl>
2022-03-08 14:25:02 +0800

    Extract SetupSpinner as component

--- frontend/src/windows/SetupWindow.tsx
+++ frontend/src/windows/SetupWindow.tsx
@@ -5,7 +5,6 @@ import {
     CardBody,
     CardFooter,
     Button,
-    ButtonVariant,
     Hint,
     HintBody,
     HelperText,
@@ -15,11 +14,6 @@ import {
     DescriptionListGroup,
     DescriptionListTerm,
     Checkbox,
-    EmptyState,
-    EmptyStateBody,
-    EmptyStateSecondaryActions,
-    EmptyStatePrimary,
-    Spinner,
     Wizard,
     WizardFooter,
     WizardContextConsumer
@@ -29,82 +23,8 @@ import {
     PullSecretInputCard
 } from '../components/PullSecretInputCard';
 import { PresetSelection } from '../components/PresetSelection';
+import { SetupSpinner } from '../components/SetupSpinner';
 
-interface SetupSpinnerProps {
-    preset: string;
-    consentTelemetry: boolean;
-    pullsecret: string;
-}
-interface SetupSpinnerState {
-    notReadyForUse: boolean;
-}
-class SetupSpinner extends React.Component<SetupSpinnerProps> {
-
-    state: Readonly<SetupSpinnerState>;
-
-    constructor(props: SetupSpinnerProps) {
-        super(props)
-
-        this.state = {
-            notReadyForUse: true,
-        };
-
-        this.handlePrimaryButtonAction = this.handlePrimaryButtonAction.bind(this);
-    }
-
-    componentDidMount() {
-        // start the crc setup process
-        // different configs needed will be passed as args
-        window.api.onSetupLogs(async (event, message) => {
-            // Do nothing
-        })
-        window.api.onSetupEnded(async (event, message) => {
-            this.setState({ notReadyForUse: false });
-        })
-        window.api.startSetup({
-            preset: this.props.preset,
-            consentTelemetry: this.props.consentTelemetry,
-            pullsecret: this.props.pullsecret
-        })
-    }
-
-    componentWillUnmount() {
-        window.api.removeSetupLogListeners();
-    }
-
-    handlePrimaryButtonAction() {
-        // issue tray start command then close window
-        window.api.closeSetupWizard();
-    }
-
-    handleDocsLinks(url: string) {
-        window.api.openLinkInDefaultBrowser(url)
-    }
-
-    render () {
-        return (
-            <EmptyState>
-                <EmptyStateBody>
-                    <div style={{height:"180px",width:"400px",marginTop:"160px"}}>
-
-                        { this.state.notReadyForUse ? <>Running setup ...<br/><br/><Spinner diameter="80px" isSVG /><br/><br/>This may take several minutes.</> : "Setup done."}
-                        
-                    </div>
-                </EmptyStateBody>
-                <EmptyStatePrimary>
-                    <Button isDisabled={this.state.notReadyForUse} variant={ButtonVariant.primary} onClick={this.handlePrimaryButtonAction}>Start using CRC</Button>
-                </EmptyStatePrimary>
-                <EmptyStateSecondaryActions>
-                    {
-                        // TOD: For now, no buttons until content is ready
-                        //<Button variant={ButtonVariant.link} onClick={ () => { this.handleDocsLinks("https://crc.dev") }}>Visit Getting started Guide</Button>
-                        //<Button variant={ButtonVariant.link} onClick={ () => { this.handleDocsLinks("https://crc.dev") }}>Example Deployments</Button>
-                    }
-                </EmptyStateSecondaryActions>
-            </EmptyState>
-        );
-    }
-}
 interface SetupWindowState {
     stepIdReached: number,
     preset: "openshift" | "podman",
